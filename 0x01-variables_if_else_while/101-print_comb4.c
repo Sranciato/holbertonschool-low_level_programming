00bb0afc6a9b01ba866dfd780388a5b069085a9a
sranciato <sranciato1@gmail.com>
2019-06-22 04:38:28 +0000

    nested for loops

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int x;
+	int a = 44;
+	int b = 32;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = (i + 1); j <= 57; j++)
+		{
+			for(x = (j + 1); x <= 57; x++)
+			{
+			putchar(i);
+			putchar(j);
+			putchar(x);
+			if (i != 55)
+			{
+				putchar(a);
+				putchar(b);
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
