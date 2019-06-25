531204eb17ee6fc00531a9f407fa2bae40bfe1d9
sranciato <sranciato1@gmail.com>
2019-06-25 01:29:34 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _islower(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
