916d8845c201f5c86ed8f202f1e55ce4b7e403ff
sranciato <sranciato1@gmail.com>
2019-06-25 17:14:04 +0000

    24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a < 3; a++)
+	{
+		for (b = 0;b < 4; b++)
+		{
+			for (c = 0; c < 6; c++)
+			{
+				for (d = 0; d < 10; d++)
+				{
+					_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(':');
+					_putchar(c + 48);
+					_putchar(d + 48);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+
+}
