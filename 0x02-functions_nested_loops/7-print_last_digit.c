9b276b0cfa93ba5ffd87572395443c3b62c304e5
sranciato <sranciato1@gmail.com>
2019-06-25 16:41:08 +0000

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_last_digit(int n)
+{
+	if (n >= 0)
+	{
+		_putchar((n % 10) + 48);
+		return (n % 10);
+	}
+	else
+	{
+		_putchar((-n % 10) + 48);
+		return (-n % 10);
+	}
+}
