79e952328599f357bcd1df24a205f4249cb83387
sranciato <sranciato1@gmail.com>
2019-06-25 21:03:25 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - writes the character c to stdout
+ * @a: The character to print
+ * @b: Character to add
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
