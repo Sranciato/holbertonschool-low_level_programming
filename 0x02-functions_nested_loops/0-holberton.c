4270aa16db8bc9da1465ba0e01439a59bf459766
sranciato <sranciato1@gmail.com>
2019-06-25 01:06:37 +0000

    putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,5 +1,4 @@
-#include <holberton.h>
-#include <stdio.h>
+#include "holberton.h"
 /**
  * main - Entry point
  *
@@ -12,8 +11,8 @@ int main(void)
 
 	for (n = 0; n < 9; n++)
 	{
-		putchar(a[n]);
+		_putchar(a[n]);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
