cab79db5b98c00ba78513445fc7c5ef5bc94b47f
sranciato <sranciato1@gmail.com>
2019-06-25 23:09:27 +0000

    fix word count

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,18 +16,25 @@ void jack_bauer(void)
 
 	for (a = 0; a < 3; a++)
 	{
-		for (b = 0; b < 4; b++)
+		for (b = 0; b < 10; b++)
 		{
 			for (c = 0; c < 6; c++)
 			{
 				for (d = 0; d < 10; d++)
 				{
-					_putchar(a + 48);
-					_putchar(b + 48);
-					_putchar(':');
-					_putchar(c + 48);
-					_putchar(d + 48);
-					_putchar('\n');
+					if (a == 2 && b > 3)
+					{
+						break;
+					}
+					else
+					{
+						_putchar(a + 48);
+						_putchar(b + 48);
+						_putchar(':');
+						_putchar(c + 48);
+						_putchar(d + 48);
+						_putchar('\n');
+					}
 				}
 			}
 		}
