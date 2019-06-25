8c2d8ff83e0538c54480438ea11d65bdf83ed72e
sranciato <sranciato1@gmail.com>
2019-06-25 12:46:00 +0000

    push header file

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,17 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * holberton.h
+ *
+ * created jun 24, 2019
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+
+#endif /* HOLBERTON_H_ */
