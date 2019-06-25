1b66510189d692040d1140ca573026839c35950e
sranciato <sranciato1@gmail.com>
2019-06-25 13:31:28 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _abs - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+	_putchar('\n');
+}
