6c11e391d0b6fb65a22e85d471bfbf7898805463
sranciato <sranciato1@gmail.com>
2019-06-26 23:25:59 +0000

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * times_table - writes the character c to stdout
- * @: The character to print
+ * print_to_98 - writes the character c to stdout
+ * @n: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (;n < 98; n++)
+		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
@@ -20,7 +20,7 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (;n > 98; n--)
+		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
