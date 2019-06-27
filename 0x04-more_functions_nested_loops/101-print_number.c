dcd840357c05118e3fbb3f1fd3814cb107141510
sranciato <sranciato1@gmail.com>
2019-06-27 18:01:12 +0000

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * print_number - check the code for Holberton School students.
+ * @n: variable name.
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	putchar(n + '0');
+}
