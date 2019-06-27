45c9904e8bb208baaef482fdf35e024036282a08
sranciato <sranciato1@gmail.com>
2019-06-27 13:30:16 +0000

    returns 1 if digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _isdigit - check the code for Holberton School students.
+ * @c: variable name.
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+	else
+		return (0);
+}
