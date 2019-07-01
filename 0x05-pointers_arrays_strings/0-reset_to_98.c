5b8d919d1a007067b7e5a5a7cdf97fcae381f1c6
sranciato <sranciato1@gmail.com>
2019-07-01 14:23:17 +0000

    add README.md header file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ * @n: variable name.
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	int *p;
+
+	p = n;
+	*p = 98;
+}
