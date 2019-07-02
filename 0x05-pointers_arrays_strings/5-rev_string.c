694b558ef6258ee4a1cc03ca2e4e113de5603ce3
sranciato <sranciato1@gmail.com>
2019-07-02 00:41:10 +0000

    print holberton backwards

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * rev_string - check the code for Holberton School students.
+ * @s: variable name.
+ */
+void rev_string(char *s)
+{
+	int size;
+	int start = 0;
+	char temp, temp2;
+
+	for (size = 0; s[size] != '\0'; size++)
+	{
+	}
+
+	size--;
+
+	while (start < size / 2)
+	{
+		temp = s[size - start];
+		temp2 = s[start];
+		s[start] = temp;
+		s[size - start] = temp2;
+		start++;
+	}
+}
