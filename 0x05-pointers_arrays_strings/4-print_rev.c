a2c3c88bebc4b5f03c03250dbdb9e2f1af7fa9e4
sranciato <sranciato1@gmail.com>
2019-07-02 14:03:15 +0000

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,9 +11,9 @@ void print_rev(char *s)
 	for (length = 0; s[length] != '\0'; length++)
 	{
 	}
-	for (; length >= 0; length--)
+	for (length = length - 1; length >= 0; length--)
 	{
-		_putchar(*(s + length));
+		_putchar(s[length]);
 	}
 	_putchar('\n');
 }
