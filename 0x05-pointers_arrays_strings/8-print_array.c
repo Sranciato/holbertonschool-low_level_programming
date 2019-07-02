c7ca1933b3e35bcfa8d324398688ea6e4655d400
sranciato <sranciato1@gmail.com>
2019-07-02 15:16:18 +0000

    print numbers in array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - check the code for Holberton School students.
+ * @a: variable name.
+ * @n: variable 2.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+	}
+	printf("\n");
+}
