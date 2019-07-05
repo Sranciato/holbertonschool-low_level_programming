54b917f4ab5b896ff77f55d43999c28a2bff2669
sranciato <sranciato1@gmail.com>
2019-07-05 21:16:18 +0000

    capitalize words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * cap_string - check the code for Holberton School students.
+ * @c: variable name.
+ * Return: Capital words.
+ */
+char *cap_string(char *c)
+{
+	int i, j, x;
+	char *s = " ,\t\n,;.!?\"(0{}";
+
+	for (i = 0; c[i] != '\0'; i++)
+		;
+
+	for (j = 0; j < i; j++)
+	{
+		if (c[j] >= 'a' && c[j] <= 'z')
+			{
+				for (x = 0; s[x] != '\0'; x++)
+				{
+					if (c[j - 1] == s[x])
+					{
+						c[j] = c[j] - 32;
+					}
+				}
+				x = 0;
+			}
+	}
+	return (c);
+}
