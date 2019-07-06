7011b2668cb4c5f62f7cda6e8e5cf87bd719ece4
sranciato <sranciato1@gmail.com>
2019-07-06 16:15:48 +0000

    fix suspect code indent

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,20 +11,19 @@ char *cap_string(char *c)
 
 	for (i = 0; c[i] != '\0'; i++)
 		;
-
 	for (j = 0; j < i; j++)
 	{
 		if (c[j] >= 'a' && c[j] <= 'z')
+		{
+			for (x = 0; s[x] != '\0'; x++)
 			{
-				for (x = 0; s[x] != '\0'; x++)
+				if (c[j - 1] == s[x])
 				{
-					if (c[j - 1] == s[x])
-					{
-						c[j] = c[j] - 32;
-					}
+					c[j] = c[j] - 32;
 				}
-				x = 0;
 			}
+			x = 0;
+		}
 	}
 	return (c);
 }
