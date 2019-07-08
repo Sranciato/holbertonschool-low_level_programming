b2f602569f3923866d9807c289595ec223e4514e
sranciato <sranciato1@gmail.com>
2019-07-08 01:40:53 +0000

    fix long number

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -23,29 +23,15 @@ void print_number(int n)
 	{
 		_putchar('0');
 	}
-	else if (n < 0 && n != -2147483648)
+	else if (n < 0)
 	{
 		_putchar('-');
 		for (y = 1000000000; y > 0; y /= 10)
 		{
-			if (-n / y != 0)
+			if (n / y != 0)
 			{
 				_putchar(((-n / y) % 10) + '0');
 			}
 		}
-	}
-	else
-	{
-		_putchar('-');
-		_putchar('2');
-		_putchar('1');
-		_putchar('4');
-		_putchar('7');
-		_putchar('4');
-		_putchar('8');
-		_putchar('3');
-		_putchar('6');
-		_putchar('4');
-		_putchar('8');
 	}
 }
