6fb876a269b922a91fc36d5750a35a64e309cfcf
sranciato <sranciato1@gmail.com>
2019-07-10 00:33:12 +0000

    fix header

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -23,5 +23,15 @@ char *cap_string(char *c);
 char *leet(char *c);
 char *rot13(char *c);
 void print_number(int n);
+/* even more pointers */
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
