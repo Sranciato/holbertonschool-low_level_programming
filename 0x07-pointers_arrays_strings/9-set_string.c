5fdbe91d20916b13b4eb3f946722d53dce8200af
sranciato <sranciato1@gmail.com>
2019-07-10 00:31:48 +0000

    set value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - check the code for Holberton School students.
+ * @s: variable name.
+ * @to: variable 2.
+ * Return: char.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
