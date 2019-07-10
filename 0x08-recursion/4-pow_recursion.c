805feb2c3836ec0208cfff02612e7f6c62e9e761
sranciato <sranciato1@gmail.com>
2019-07-10 18:53:43 +0000

    add power of recursive

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - check the code for Holberton School students.
+ * @x: variable name.
+ * @y: variable 2.
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
+}
