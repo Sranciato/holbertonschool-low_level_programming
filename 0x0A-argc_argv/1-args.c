dbec3b015489b35c75d53b96b79fb6d25f1ddb0a
sranciato <sranciato1@gmail.com>
2019-07-12 23:03:32 +0000

    add argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: variable name.
+ * @argv: variable 2.
+ * Return: int.
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d", argc - 1);
+
+	return (0);
+}
