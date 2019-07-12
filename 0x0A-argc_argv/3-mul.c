f3c79f3e72f5ee89842123fdb3e67d5758bb09b0
sranciato <sranciato1@gmail.com>
2019-07-12 23:38:30 +0000

    add multiplication of commands

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: variable name.
+ * @argv: variable 2.
+ * Return: int.
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc > 1)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+		return (0);
+	}
+	printf("ERROR\n");
+	return (1);
+
+}
