a83db5df4f4c61ded755e733027267d832f7a14b
sranciato <sranciato1@gmail.com>
2019-07-13 00:01:34 +0000

    add correct library to file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: variable name.
+ * @argv: variable 2.
+ * Return: int.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) > 0)
+				sum += atoi(argv[i]);
+			else if (atoi(argv[i]) == 0)
+			{
+				printf("ERROR");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	printf("0\n");
+	return (0);
+}
