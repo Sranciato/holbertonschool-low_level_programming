7331fc595534bc0fa928de522a968bda6e32ce60
sranciato <sranciato1@gmail.com>
2019-07-16 18:13:24 +0000

    fix if null is passed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,20 +12,24 @@ char *str_concat(char *s1, char *s2)
 	int i, j, s1len, s2len;
 	char *p;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+	{
+		s1 = malloc(sizeof(char));
+		s1[0] = '\0';
+	}
+	if (s2 == NULL)
+		s2 = malloc(sizeof(char));
+		s2[0] = '\0';
 	for (s1len = 0; s1[s1len] != '\0'; s1len++)
 		;
 	for (s2len = 0; s2[s2len] != '\0'; s2len++)
 		;
 	p = malloc(sizeof(char) * (s1len + s2len) + 1);
+	for (i = 0; i < s1len; i++)
+		p[i] = s1[i];
+	for (j = 0; j <= s2len; j++)
+		p[s1len + j] = s2[j];
 	if (p)
-	{
-		for (i = 0; i < s1len; i++)
-			p[i] = s1[i];
-		for (j = 0; j <= s2len; j++)
-			p[s1len + j] = s2[j];
 		return (p);
-	}
 	return (NULL);
 }
