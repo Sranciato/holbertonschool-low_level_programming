40d9be1e65389e14c951cb44bce3f7942302b451
sranciato <sranciato1@gmail.com>
2019-07-16 16:56:57 +0000

    fix memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *_strdup(char *str)
 	for (len = 0; str[len] != '\0'; len++)
 		;
 	p = malloc(sizeof(char) * len + 1);
-	for (i = 0; i <= len + 1; i++)
+	for (i = 0; i <= len; i++)
 		p[i] = str[i];
 	return (p);
 }
