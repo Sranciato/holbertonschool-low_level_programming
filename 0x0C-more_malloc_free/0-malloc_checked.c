5c6ff6e9e3b6cc785bc79127503c651a5305a46d
sranciato <sranciato1@gmail.com>
2019-07-18 16:16:32 +0000

    fix exit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - check the code for Holberton School students.
+ * @b: variable name.
+ * Return: pointer.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
