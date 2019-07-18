a194654f8ef1c249d2a4f2cdbbe0a4185330660f
sranciato <sranciato1@gmail.com>
2019-07-18 21:07:58 +0000

    fix header

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *p;
 
 	if (new_size <= old_size)
-		return (ptr);
+                return (ptr);
 	if (new_size == 0 && ptr)
 	{
 		free(ptr);
@@ -28,8 +28,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 		}
 	}
-	if (new_size < oldsize)
-		p = malloc(
 	p = malloc(new_size);
 	if (p == NULL)
 		return (NULL);
