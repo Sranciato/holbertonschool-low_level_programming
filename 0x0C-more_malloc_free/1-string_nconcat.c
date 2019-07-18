94625bc942c336f60a45cd32c44011a4e4deef16
sranciato <sranciato1@gmail.com>
2019-07-18 16:36:02 +0000

    fix free

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * string_nconcat - check the code for Holberton School students.
+ * @s1: variable name.
+ * @s2: variable 2.
+ * @n: variable 3.
+ * Return: pointer.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, s1len, s2len;
+	char *p;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (s1len = 0; s1[s1len] != '\0'; s1len++)
+		;
+	for (s2len = 0; s2[s2len] != '\0'; s2len++)
+		;
+	p = malloc(sizeof(char) * (s1len + n) + 1);
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	for (i = 0; i <= s1len; i++)
+		p[i] = s1[i];
+	if (n >= s2len)
+		n = s2len;
+	for (j = 0; j <= n; j++)
+		p[s1len + j] = s2[j];
+	return (p);
+}
