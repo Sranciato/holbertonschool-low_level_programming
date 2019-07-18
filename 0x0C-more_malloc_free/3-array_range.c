7b9eea84e9af90910a74e1a8c93dd57d5df3ae19
sranciato <sranciato1@gmail.com>
2019-07-18 19:01:41 +0000

    fix error

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,9 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	p = malloc(sizeof(int) * (max - min) + 1);
+	p = malloc(sizeof(int) * (max - min + 1));
+	if (p == NULL)
+		return (NULL);
 	for (i = 0; min <= max; i++)
 	{
 		p[i] = min;
