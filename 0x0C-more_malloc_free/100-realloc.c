ee30f693785b9224b1e9080b62bafdce00b9363b
sranciato <sranciato1@gmail.com>
2019-07-18 20:58:06 +0000

    fix header

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _realloc - check the code for Holberton School students.
+ * @ptr: variable name.
+ * @old_size: variable 2.
+ * @new_size: variable 3.
+ * Return: pointer.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	char *p;
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+	}
+	if (new_size == old_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < old_size; i++)
+		p[i] = ((char *)ptr)[i];
+	free(ptr);
+	return (p);
+}
