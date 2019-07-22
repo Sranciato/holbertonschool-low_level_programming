837158871b2a0b75115e65fdbd4533076da244dc
sranciato <sranciato1@gmail.com>
2019-07-22 17:28:24 +0000

    fix ->syntax

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - writes the character c to stdout
+ * @d: The character to print.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
