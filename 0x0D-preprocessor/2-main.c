207fdba2e879f5667459f3b40677aa825eed10dc
sranciato <sranciato1@gmail.com>
2019-07-22 16:21:10 +0000

    fix betty

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,10 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * Return: pointer.
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
