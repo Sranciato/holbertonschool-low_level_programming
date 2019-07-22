6b191638bb149dac3edcd70d2629b8820365e46c
sranciato <sranciato1@gmail.com>
2019-07-22 20:58:36 +0000

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,11 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_dog - check the code for Holberton School students.
+ * @d: variable name.
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
