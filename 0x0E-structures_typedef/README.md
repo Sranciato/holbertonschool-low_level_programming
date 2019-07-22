f1a1dca826461c6b43dfc2f19de0bb863103b5e6
sranciato <sranciato1@gmail.com>
2019-07-22 16:50:24 +0000

    create struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+**Making structures and learning typedef**
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+int _putchar(char c);
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
