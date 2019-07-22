125d2250cc60716b0bf05250e1f0fa427ff3bcc9
sranciato <sranciato1@gmail.com>
2019-07-22 17:27:52 +0000

    fix header file prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,6 @@
 #define _DOG_H_
 
 int _putchar(char c);
-
 /**
  * struct dog - Short description
  * @name: First member
@@ -17,5 +16,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
