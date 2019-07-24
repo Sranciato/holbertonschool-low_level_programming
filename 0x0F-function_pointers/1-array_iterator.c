44dab5ed61b59113bae2c905612a567ac95d14dd
sranciato <sranciato1@gmail.com>
2019-07-24 16:41:28 +0000

    add prototype

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,10 +9,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array)
+	if (array && action)
 	{
 		for (i = 0; i < size; i++)
-			if (array[i] >= 0)
-				action(array[i]);
+			action(array[i]);
 	}
 }
