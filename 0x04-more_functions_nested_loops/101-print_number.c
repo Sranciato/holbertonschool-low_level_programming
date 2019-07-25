738d60497d1503adf49e2d93611f7c99973f02c3
sranciato <sranciato1@gmail.com>
2019-07-25 21:25:33 +0000

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,35 @@
  */
 void print_number(int n)
 {
-	_putchar(n + '0');
-	_putchar('\n');
+	int y;
+
+	if (n > 0)
+	{
+		n *= -1;
+		for (y = 1000000000; y > 0; y /= 10)
+		{
+			if (n / y != 0)
+			{
+				_putchar(((-n / y) % 10) + '0');
+			}
+		}
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		for (y = 1000000000; y > 0; y /= 10)
+		{
+			if (n / y != 0)
+			{
+				if (n < -2147483647)
+					_putchar((((-n / y) % 10) * -1) + '0');
+				else
+					_putchar(((-n / y) % 10) + '0');
+			}
+		}
+	}
 }
