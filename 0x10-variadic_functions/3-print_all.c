e1d95fcba8df4de63793a5096480f81db320f056
sranciato <sranciato1@gmail.com>
2019-07-26 17:28:57 +0000

    mini printf

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,61 @@
+#include "variadic_functions.h"
+
+void checker(int a, int i);
+
+/**
+ * checker - print anything.
+ * @a: check.
+ * @i: i value
+ */
+void checker(int a, int i)
+{
+	if (a == 1 && i > 0)
+	{
+		printf(", ");
+	}
+}
+/**
+ * print_all - print anything.
+ * @format: variable types.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	char *string;
+	int check = 0, i = 0;
+
+	va_start(valist, format);
+
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case 'c':
+			checker(check, i);
+			printf("%c", va_arg(valist, int));
+			check = 1;
+			break;
+		case 'i':
+			checker(check, i);
+			printf("%i", va_arg(valist, int));
+			check = 1;
+			break;
+		case 'f':
+			checker(check, i);
+			printf("%f", va_arg(valist, double));
+			check = 1;
+			break;
+		case 's':
+			checker(check, i);
+			string = va_arg(valist, char *);
+			if (string == NULL)
+				string = "(nil)";
+			printf("%s", string);
+			check = 1;
+			break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(valist);
+}
