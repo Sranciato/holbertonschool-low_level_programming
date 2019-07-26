1e99465d5282383a13e67db087642c75f2ebdf54
sranciato <sranciato1@gmail.com>
2019-07-26 15:17:03 +0000

    added prototype

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,11 @@
 #ifndef V_FUNCTIONS_
 #define V_FUNCTIONS_
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
