2c1af4ed762cbb5733b34a5c7e320a164702f320
sranciato <sranciato1@gmail.com>
2019-07-28 01:52:34 +0000

    add if number is negative

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,7 +11,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(valist, n);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && n > 0; i++)
 	{
 		if (i > 0)
 			printf("%s%i", separator, va_arg(valist, unsigned int));
