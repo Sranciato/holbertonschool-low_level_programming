68d2b179b3424f98d5d2b51a83b1e3d294cedb2c
sranciato <sranciato1@gmail.com>
2019-07-29 00:47:40 +0000

    fix if format specifeires are null

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -25,41 +25,38 @@ void print_all(const char * const format, ...)
 	char *string;
 	int check = 0, i = 0;
 
-	if (format)
-	{
-		va_start(valist, format);
+	va_start(valist, format);
 
-		while (format[i] != '\0')
+	while (format && format[i] != '\0')
+	{
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-			case 'c':
-				checker(check, i);
-				printf("%c", va_arg(valist, int));
-				check = 1;
-				break;
-			case 'i':
-				checker(check, i);
-				printf("%i", va_arg(valist, int));
-				check = 1;
-				break;
-			case 'f':
-				checker(check, i);
-				printf("%f", va_arg(valist, double));
-				check = 1;
-				break;
-			case 's':
-				checker(check, i);
-				string = va_arg(valist, char *);
-				if (string == NULL)
-					string = "(nil)";
-				printf("%s", string);
-				check = 1;
-				break;
-			}
-			i++;
+		case 'c':
+			checker(check, i);
+			printf("%c", va_arg(valist, int));
+			check = 1;
+			break;
+		case 'i':
+			checker(check, i);
+			printf("%i", va_arg(valist, int));
+			check = 1;
+			break;
+		case 'f':
+			checker(check, i);
+			printf("%f", va_arg(valist, double));
+			check = 1;
+			break;
+		case 's':
+			checker(check, i);
+			string = va_arg(valist, char *);
+			if (string == NULL)
+				string = "(nil)";
+			printf("%s", string);
+			check = 1;
+			break;
 		}
-		printf("\n");
-		va_end(valist);
+		i++;
 	}
+	printf("\n");
+	va_end(valist);
 }
