2f6a92badf260ac82af164df6f5a6cd2bb208f91
sranciato <sranciato1@gmail.com>
2019-08-01 19:18:15 +0000

    fix list printing backwards

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * add_node - print a linked list.
+ * @head: pointer to list.
+ * @str: string of struct.
+ * Return: list_t.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int length;
+	list_t *new;
+	char *string = strdup(str);
+
+
+	for (length = 0; string[length] != '\0'; length++)
+		;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->len = length;
+	new->str = string;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
