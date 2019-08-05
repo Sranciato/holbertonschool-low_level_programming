6d01b01d43f63cabb59bf7685e8ab8fae4bf0b4f
sranciato <sranciato1@gmail.com>
2019-08-05 18:05:36 +0000

    Sum of all data.

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_listint - return sum of all data.
+ * @head: Start of list.
+ * Return: int.
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *search = head;
+	int sum;
+
+	if (search == NULL)
+		return (0);
+	while (search)
+	{
+		sum += search->n;
+		search = search->next;
+	}
+	return (sum);
+}
