251c524e23ad4eb27f05f9a513a0934d164eceab
sranciato <sranciato1@gmail.com>
2019-08-08 15:12:09 +0000

    set value to 0 at given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * clear_bit - print the binary of a given number.
+ * @n: number to be converted.
+ * @index: index where binary 1 will be added.
+ * Return: 1 if success. -1 if error.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	mask = 1 << index;
+	*n = ((*n & ~mask) | ((0 << index) & mask));
+	if (!n)
+		return (-1);
+	return (1);
+}
