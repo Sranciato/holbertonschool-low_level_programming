0d3efedd3b9b337fc9cde21d135c59a15fff44d7
sranciato <sranciato1@gmail.com>
2019-08-08 16:06:22 +0000

    fix if index is greater than amount of bits possible (64)

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,7 +8,8 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
-
+	if (index > 63)
+		return (-1);
 	mask = 1 << index;
 	*n = ((*n & ~mask) | ((1 << index) & mask));
 	if (!n)
