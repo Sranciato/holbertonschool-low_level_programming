1a255cf9b311ab363026e115d9f6369f8fa1d274
sranciato <sranciato1@gmail.com>
2019-08-08 15:00:04 +0000

    set value to 1 at given index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * set_bit - print the binary of a given number.
+ * @n: number to be converted.
+ * @index: index where binary 1 will be added.
+ * Return: 1 if success. -1 if error.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	mask = 1 << index;
+	*n = ((*n & ~mask) | ((1 << index) & mask));
+	if (!n)
+		return (-1);
+	return (1);
+}
