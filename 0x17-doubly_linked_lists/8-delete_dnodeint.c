e61b355ea268425dda5ea9a246c73bb222083da4
sranciato <sranciato1@gmail.com>
2019-09-15 17:35:34 +0000

    [LOCAL][INIT] free node at given index

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes node at given index.
+ * @head: beginning of list.
+ * @index: index of node to be deleted.
+ * Return: 1 on success, 0 on failure.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *search = *head;
+	unsigned int i = 0;
+
+	while (search)
+	{
+		if (i == index)
+		{
+			if (search->next)
+				search->next->prev = search->prev;
+			if (index > 0)
+				search->prev->next = search->next;
+			else if (index == 0)
+			{
+				search = (*head);
+				(*head) = search->next;
+			}
+			free(search);
+			return (1);
+		}
+		search = search->next;
+		i++;
+	}
+	return (0);
+}
