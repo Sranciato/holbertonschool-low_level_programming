2a18df196a31ed994bcce3e833fd273acc36bdb8
sranciato <sranciato1@gmail.com>
2019-10-11 19:50:59 +0000

    [BUGFIX] add pointer at end of hash_table_t

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!new)
 		return (NULL);
 
-	new->array = malloc(size * sizeof(hash_node_t));
+	new->array = malloc(size * sizeof(hash_node_t *));
 	if (!new->array)
 	{
 		free(new);
