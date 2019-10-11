65317b5104000910f750532ea0282c637db8eae8
sranciato <sranciato1@gmail.com>
2019-10-11 21:50:24 +0000

    [BUGFIX] check for empty string

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,9 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
 
-	if (!ht || !key || !value)
-		return (0);
-	if (*key == '\0')
+	if (!ht || !key || !value || strcmp(key, "") == 0)
 		return (0);
 
 	new_node = malloc(sizeof(hash_node_t));
