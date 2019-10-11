7c33468e73a03077526f71650eeeff95ca213ff4
sranciato <sranciato1@gmail.com>
2019-10-11 23:54:16 +0000

    [FEATURE] add free hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - free hash table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *temp;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			while (ht->array[i])
+			{
+				temp = ht->array[i]->next;
+				free(ht->array[i]->key);
+				free(ht->array[i]->value);
+				free(ht->array[i]);
+				ht->array[i] = temp;
+			}
+			free(ht->array[i]);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
