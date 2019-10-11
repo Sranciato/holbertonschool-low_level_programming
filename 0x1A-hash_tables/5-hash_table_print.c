247aa33834c3fe1820063b25067354b57bb0538a
sranciato <sranciato1@gmail.com>
2019-10-11 23:40:50 +0000

    [FEATURE] add print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	int count = 0;
+
+	if (ht)
+		printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (count > 0)
+				printf(", ");
+			printf("\'%s\': \'%s\'", node->key, node->value);
+			node = node->next;
+			count++;
+		}
+	}
+	printf("}\n");
+}
