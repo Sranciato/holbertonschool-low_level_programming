69b9c9fbeb85d78410c19770e44c5945a0f9b876
sranciato <sranciato1@gmail.com>
2019-10-11 21:32:15 +0000

    [BUGFIX] got rid of strcmp check in beggining

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,8 +14,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key || !value)
 		return (0);
-	if (strcmp(key, "") != 0)
-		return (0);
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
