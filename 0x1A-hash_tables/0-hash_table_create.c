3f4b8d54e99609e3dcba3246c30ca92b349381dd
sranciato <sranciato1@gmail.com>
2019-10-11 19:44:59 +0000

    [INIT] first push

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - create an empty hash table
+ * @size: size of hash table
+ * Return: pointer to new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	new = malloc(sizeof(hash_table_t));
+	if (!new)
+		return (NULL);
+
+	new->array = malloc(size * sizeof(hash_node_t));
+	if (!new->array)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->size = size;
+
+	for (i = 0; i < size; i++)
+		new->array[i] = NULL;
+
+	return (new);
+}
