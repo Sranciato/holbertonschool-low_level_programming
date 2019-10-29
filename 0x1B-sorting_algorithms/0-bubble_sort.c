25ffadf338c2b7eaa15cb5b17867e969efb2bc4b
sranciato <sranciato1@gmail.com>
2019-10-29 17:20:22 +0000

    [FEATURE] add 0, 1, 3, 101 for checker

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+/**
+ * bubble_sort - assort integers in ascending order.
+ * @array: array of integers.
+ * @size: size of array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i;
+	int length = size;
+
+	for (size -= 1; size > 0; size--)
+	{
+		for (i = 0; i < size; i++)
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, length);
+			}
+	}
+}
