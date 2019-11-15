6ac628e89a41f9005071a13d43e7ab572e0565e6
sranciato <sranciato1@gmail.com>
2019-11-15 21:52:56 +0000

    [FIX] fix if corner add both side lengths

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,22 +8,16 @@ def island_perimeter(grid):
 
     for lis in range(len(grid)):
         for val in range(len(grid[lis])):
-            if check == 1:
-                check = 0
-                continue
             if grid[lis][val] == 1:
                 try:
                     if grid[lis - 1][val] == 0:
                         count += 1
                     if grid[lis][val - 1] == 0:
-                        grid[lis][val - 1] = 1
                         count += 1
                     if grid[lis][val + 1] == 0:
-                        grid[lis][val + 1] = 1
-                        check = 1
                         count += 1
                     if grid[lis + 1][val] == 0:
                         count += 1
                 except:
                     pass
-    return count + 1
+    return count
