9482c43761b8451a324fd7ea3f23b99b0daad569
sranciato <sranciato1@gmail.com>
2019-12-05 02:33:43 +0000

    [ADD] tasks 4-13

---
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - height of binary tree.
+ * @tree: binary tree.
+ * Return: height of binary tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (tree == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left > right)
+	{
+		if (!tree->left)
+			return (left);
+		return (left + 1);
+	}
+	else
+	{
+		if (!tree->right)
+			return (right);
+		return (right + 1);
+	}
+}
